796f9a3b7f426c61e63e88c519a9fdc4a5b0f6df
Clock <double7@mail.ru>
2017-01-28 13:24:56 +0700

    ++

--- apt-get.sh
+++ apt-get.sh
@@ -25,6 +25,7 @@ sudo apt-get install -y libreoffice
 sudo apt-get install -y wireshark
 sudo apt-get install -y handbrake openshot frei0r-plugins inkscape
 #sudo apt-get install -y ubuntu-restricted-extras
+sudo apt-get install -y vokoscreen recordmydesktop gtk-recordmydesktop #qt-recordmydesktop
 sudo apt-get install -y samba system-config-samba
 sudo apt-get install -y audacity audacity-data
 #sudo apt-get install -y libkworkspace5-5

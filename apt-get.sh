c3a9ea69a2421c5dba17c830ad061bdd608716ac
Clock <double7@mail.ru>
2017-05-14 00:51:20 +0700

    + g++ gcc

--- apt-get.sh
+++ apt-get.sh
@@ -21,7 +21,7 @@ sudo apt-get install -y gksu gdebi
 sudo apt-get install -y playonlinux
 sudo apt-get install -y ktorrent qbittorrent
 #sudo apt-get install -y nemo gnome-terminal
-sudo apt-get install -y git gitk
+sudo apt-get install -y git g++ gcc gitk
 #sudo apt-get install -y gedit-dev gedit-common gedit-plugins #gedit-developer-plugins
 sudo apt-get install -y geany kate kate-data kate5-data
 sudo apt-get install -y nmap

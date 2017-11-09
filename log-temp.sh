c9e3aa1bcc5ab1ecdaec0111ae15dcf7de6f9c23
Clock <double7@mail.ru>
2017-11-09 20:20:49 +0700

    some cosmetic features

--- log-temp.sh
+++ log-temp.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 i=0
-printf 'type Ctrl+C to stop'
-printf 'Begin: ' > log.txt
-date | awk '{print $3,$2,$4}' >> log.txt
+echo 'type Ctrl+C to stop'
+printf 'Begin: '
+date | awk '{print $3,$2,$4}'
 
 while true
 do
-	echo $i >> log.txt
+	echo $i
 	((i++))
-	echo '-----------------------' >> log.txt
-	date >> log.txt
-	sensors | awk "NR==3; NR==7; NR==8" >> log.txt
-	echo '-----------------------' >> log.txt
-	echo '' >> log.txt
+	echo '-----------------------'
+	date
+	sensors | awk "NR==3; NR==7; NR==8"
+	echo '-----------------------'
+	echo ''
 	sleep 1
 done
